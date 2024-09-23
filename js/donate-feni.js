2c6759b1055b8de0e6779b8fe88b14fb0cc37354
R-RafiAhmed <rafiahamed7706@gmail.com>
2024-09-24 04:24:20 +0600

    added feni flood donation part

--- js/donate-feni.js
+++ js/donate-feni.js
@@ -1,19 +1,18 @@
-document.getElementById('donate-noakhali').addEventListener('click', function(){
-
-    const donateAmount = getValueById('donate-amount');
-    const donatedAmountBDT = getTextById('donated-amount');
+document.getElementById('donate-feni').addEventListener('click', function(){
+    
+    const donateAmount = getValueById('donate-amount-feni');
+    const donatedAmountBDT = getTextById('donated-amount-feni');
     const accountBalance = getTextById('account-balance');
 
     if(!isNaN(donateAmount) && donateAmount > 0){
         const addedAmount = donatedAmountBDT + donateAmount;
-        document.getElementById('donated-amount').innerText = addedAmount;
+        document.getElementById('donated-amount-feni').innerText = addedAmount;
 
         const newBalance = accountBalance - donateAmount;
         document.getElementById('account-balance').innerText = newBalance;
         alert('Congratulation! You have donated for Flood at Noakhali.');
     }
     else{
-        alert('Failed to donate.');
+        alert('Failed to donate.')
     }
-
 })
