3bfa3f8012db8663ffe6a10d44f7da50eb5f94ed
R-RafiAhmed <rafiahamed7706@gmail.com>
2024-09-24 21:53:19 +0600

    added quota part and applyed money donation system

--- js/donate-quota.js
+++ js/donate-quota.js
@@ -1,7 +1,7 @@
-document.getElementById('donate-feni').addEventListener('click', function(){
+document.getElementById('donate-quota').addEventListener('click', function(){
     
-    const donateAmount = getValueById('donate-amount-feni');
-    const donatedAmountBDT = getTextById('donated-amount-feni');
+    const donateAmount = getValueById('donate-amount-quota');
+    const donatedAmountBDT = getTextById('donated-amount-quota');
     const accountBalance = getTextById('account-balance');
 
     if(accountBalance < 0){
@@ -15,22 +15,24 @@ document.getElementById('donate-feni').addEventListener('click', function(){
             return;
         }
         const addedAmount = donatedAmountBDT + donateAmount;
-        document.getElementById('donated-amount-feni').innerText = addedAmount;
+        document.getElementById('donated-amount-quota').innerText = addedAmount;
 
         const newBalance = accountBalance - donateAmount;
         document.getElementById('account-balance').innerText = newBalance;
-        alert('Congratulation! You have donated for Flood at Feni.');
+        alert('Congratulation! You have donated for Aid for Injured in the Quota Movement.');
 
         // donation history
         const historyCard = document.createElement('div');
         historyCard.className = 'p-8 border-2 rounded-2xl mb-6';
         historyCard.innerHTML = `
-            <h3 class="text-xl font-bold leading-8">${donateAmount} Taka is Donated for Flood Relief in Feni, Bangladesh.</h3>
+            <h3 class="text-xl font-bold leading-8">${donateAmount} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh.</h3>
             <p>Date : ${new Date()}</p>
         `;
         document.getElementById('historys').appendChild(historyCard);
     }
     else{
-        alert('Failed to donate.')
+        alert('Failed to donate.');
     }
+
+    console.log(donateAmount, donatedAmountBDT, accountBalance);
 })
